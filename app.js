d8c44779a898cb49d0b0b7b008a3d79c33570d37
KaterynaMishchuk12 <138882300+KaterynaMishchuk12@users.noreply.github.com>
2024-04-18 16:16:23 +0300

    Jimp doesn`t work corr

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import "dotenv/config";
+import multer from "multer";
+import path from "path";
+
 import contactsRouter from "./routes/contactsRouter.js";
 import usersRouter from "./routes/userRouter.js";
 
@@ -11,7 +14,16 @@ app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
 
-app.use("/users", usersRouter);
+const tempDir = path.join(_dirname, "temp");
+
+const multerConfig = multer.diskStorage({
+  destination: tempDir,
+});
+const upload = multer({
+  storage: multerConfig,
+});
+
+app.use("/users", upload, usersRouter);
 app.use("/api/contacts", contactsRouter);
 
 app.use((_, res) => {
